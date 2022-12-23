ba02266edaecd456166d0e8b2a39b03751956370
ingalls <nick@ingalls.ca>
2022-12-23 09:29:51 -0400

    Download, Extract, and cleanup

--- ingest-zip/index.js
+++ ingest-zip/index.js
@@ -1,7 +1,8 @@
+import crypto from 'crypto';
 import os from 'os';
 import path from 'path';
 import AWS from 'aws-sdk';
-import fs from 'fs/promises';
+import fs from 'fs';
 import { pipeline } from 'stream/promises';
 import Zip from 'adm-zip';
 
@@ -10,7 +11,7 @@ const s3 = new AWS.S3({ region: process.env.AWS_DEFAULT_REGION || 'us-east-1' })
 async function handler() {
     const task = JSON.parse(process.env.TASK);
 
-    await promise(
+    await pipeline(
         s3.getObject({
             Bucket: task.Bucket,
             Key: task.Key
@@ -18,11 +19,30 @@ async function handler() {
        fs.createWriteStream(path.resolve(os.tmpdir(), 'input.zip'))
    );
 
+   const batch = `batch-${crypto.randomUUID()}`;
+
     const zip = new Zip(path.resolve(os.tmpdir(), 'input.zip'));
 
     for (const entry of zip.getEntries()) {
-        console.error(entry);
+        const {ext} = path.parse(entry.entryName);
+        const data = entry.getData();
+        // Ensure if there are images with the same name they don't clobber on s3
+        const key = crypto.createHash('md5').update(data).digest('hex');
+
+        console.log(`ok - writing: ${batch}/${key}${ext}`);
+        await s3.putObject({
+            Bucket: task.Bucket,
+            Key: `${batch}/${key}${ext}`,
+            Body: data
+        }).promise();
     }
+
+    await s3.deleteObject({
+        Bucket: task.Bucket,
+        Key: task.Key
+    }).promise();
+
+    console.log('ok - extraction complete');
 }
 
 if (import.meta.url === `file://${process.argv[1]}`) handler();
