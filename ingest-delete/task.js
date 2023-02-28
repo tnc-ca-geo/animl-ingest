464c59e9dd22d77c01f4b1bf0ea6914be961e202
ingalls <nick@ingalls.ca>
2023-02-28 15:09:12 -0800

    More Ingest Delete Mods

--- ingest-delete/task.js
+++ ingest-delete/task.js
@@ -1,20 +1,18 @@
 import CloudFormation from '@aws-sdk/client-cloudformation';
 
-export default class Task {
-    static async handler(payload) {
-        const cf = new CloudFormation.CloudFormationClient({ region: process.env.AWS_DEFAULT_REGION || 'us-east-1' });
+export default async function handler(payload) {
+    const cf = new CloudFormation.CloudFormationClient({ region: process.env.AWS_DEFAULT_REGION || 'us-east-1' });
 
-        console.error(payload);
-        const batch = payload.batch;
+    console.error(payload);
+    const batch = payload.batch;
 
-        await cf.send(new CloudFormation.DeleteStackCommand({
-            StackName: `${process.env.StackName}-${batch}`
-        }));
+    await cf.send(new CloudFormation.DeleteStackCommand({
+        StackName: `${process.env.StackName}-${batch}`
+    }));
 
-        console.log('ok - stack deletion complete');
-    }
+    console.log('ok - stack deletion complete');
 }
 
 if (import.meta.url === `file://${process.argv[1]}`) {
-    await Task.handler();
+    await handler();
 }
