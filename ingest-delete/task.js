c2f032c33910acbb84769e4d7d58f8dc82eaf01e
ingalls <nick@ingalls.ca>
2023-02-28 15:48:58 -0800

    Log SNS

--- ingest-delete/task.js
+++ ingest-delete/task.js
@@ -1,10 +1,10 @@
 import CloudFormation from '@aws-sdk/client-cloudformation';
 
-export async function handler(payload) {
+export async function handler(event) {
     const cf = new CloudFormation.CloudFormationClient({ region: process.env.AWS_DEFAULT_REGION || 'us-east-1' });
 
-    console.error(payload);
-    const batch = payload.batch;
+    console.error(JSON.stringify(event.Records[0].Sns));
+    const batch = event.batch;
 
     await cf.send(new CloudFormation.DeleteStackCommand({
         StackName: `${process.env.StackName}-${batch}`
