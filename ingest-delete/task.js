ca7e47f50abe8cd7cd8919e2d5e7e416e910bb3b
ingalls <nick@ingalls.ca>
2023-02-28 14:40:09 -0800

    Migrate ingest-delete to lambda runtime

---
+++ ingest-delete/task.js
@@ -0,0 +1,20 @@
+import CloudFormation from '@aws-sdk/client-cloudformation';
+
+export default class Task {
+    static async handler(payload) {
+        const cf = new CloudFormation.CloudFormationClient({ region: process.env.AWS_DEFAULT_REGION || 'us-east-1' });
+
+        console.error(payload);
+        const batch = payload.batch;
+
+        await cf.send(new CloudFormation.DeleteStackCommand({
+            StackName: `${process.env.StackName}-${batch}`
+        }));
+
+        console.log('ok - stack deletion complete');
+    }
+}
+
+if (import.meta.url === `file://${process.argv[1]}`) {
+    await Task.handler();
+}
