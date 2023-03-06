461c6e0557f0537808557754918ad8a7527cd9e6
ingalls <nick@ingalls.ca>
2023-03-06 07:54:53 -0700

    Setup SSM for API

--- ingest-zip/index.js
+++ ingest-zip/index.js
@@ -4,13 +4,31 @@ import path from 'path';
 import fs from 'fs';
 import { pipeline } from 'stream/promises';
 import S3 from '@aws-sdk/client-s3';
+import SSM from '@aws-sdk/client-ssm';
 import CloudFormation from '@aws-sdk/client-cloudformation';
 import Zip from 'adm-zip';
 import Stack from './lib/stack.js';
 
+const APIKEY = process.env.APIKEY;
+const QUERY = `
+    mutation CreateBatch($input: CreateBatch!){
+    }
+`;
+
 export default async function handler() {
     const s3 = new S3.S3Client({ region: process.env.AWS_DEFAULT_REGION || 'us-east-1' });
     const cf = new CloudFormation.CloudFormationClient({ region: process.env.AWS_DEFAULT_REGION || 'us-east-1' });
+    const ssm = new SSM.SSMClient({ region: process.env.AWS_DEFAULT_REGION || 'us-east-1' });
+
+    const params = new Map();
+    params.set(`/api/url-${process.env.STAGE}`, 'ANIML_API_URL');
+
+    for (const param of (await ssm.send(new SSM.GetParametersCommand({
+        Names: Array.from(SSM.keys()),
+        WithDecryption: true
+    }))).Parameters) {
+        params.set(param.Name, param.Value);
+    }
 
     const task = JSON.parse(process.env.TASK);
 
@@ -38,6 +56,45 @@ export default async function handler() {
     }));
 
     console.log('ok - created batch stack');
+
+    console.log(`Posting metadata to API: ${md}`);
+
+    const res = await fetch(params.get(`/api/url-${process.env.STAGE}`), {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'x-api-key': APIKEY
+        },
+        body: JSON.stringify({
+            query: QUERY,
+            variables: {
+                input: {
+                    md: md
+                }
+            }
+        })
+    });
+
+    if (!res.ok) {
+        const texterr = await res.text();
+        let jsonerr;
+        try {
+            jsonerr = JSON.parse(texterr);
+        } catch (err) {
+            throw new Error(texterr);
+        }
+
+        if (jsonerr.message) throw new Error(jsonerr.message);
+        throw new Error(texterr);
+    }
+
+    const json = await res.json();
+
+    if (json && Array.isArray(json.errors) && json.errors.length && json.errors[0].message.includes('E11000')) {
+        throw new Error('DUPLICATE_IMAGE');
+    } else if (json && Array.isArray(json.errors) && json.errors.length) {
+        throw new Error(json.errors[0].message);
+    }
 
     const zip = new Zip(path.resolve(os.tmpdir(), 'input.zip'));
 
