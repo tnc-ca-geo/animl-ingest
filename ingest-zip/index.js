806607cb920d9cd6c3477dce2c7efd67b8fed6e8
ingalls <nick@ingalls.ca>
2023-03-08 08:45:51 -0700

    Move GraphQL calls into single fn for error handling

--- ingest-zip/index.js
+++ ingest-zip/index.js
@@ -10,16 +10,6 @@ import Zip from 'adm-zip';
 import Stack from './lib/stack.js';
 
 const APIKEY = process.env.APIKEY;
-const QUERY = `
-    mutation CreateBatchRecord($input: CreateBatchInput!){
-        createBatch(input: $input) {
-            batch {
-                _id
-                processing_start
-            }
-        }
-    }
-`;
 
 export default async function handler() {
     const s3 = new S3.S3Client({ region: process.env.AWS_DEFAULT_REGION || 'us-east-1' });
@@ -64,6 +54,77 @@ export default async function handler() {
 
     console.log('ok - created batch stack');
 
+    await fetcher({
+        body: JSON.stringify({
+            query: `
+                mutation CreateBatch($input: CreateBatchInput!){
+                    createBatch(input: $input) {
+                        batch {
+                            _id
+                            processingStart
+                        }
+                    }
+                }
+            `,
+            variables: {
+                input: {
+                    _id: batch,
+                    processingStart: new Date()
+                }
+            }
+        })
+    })
+
+    const zip = new Zip(path.resolve(os.tmpdir(), 'input.zip'));
+
+    let total = 0;
+    for (const entry of zip.getEntries()) {
+        const { ext } = path.parse(entry.entryName);
+        if (!ext) continue;
+
+        const data = entry.getData();
+        // Ensure if there are images with the same name they don't clobber on s3
+        const key = crypto.createHash('md5').update(data).digest('hex');
+
+        console.log(`ok - writing: ${batch}/${key}${ext}`);
+        await s3.send(new S3.PutObjectCommand({
+            Bucket: task.Bucket,
+            Key: `${batch}/${key}${ext}`,
+            Body: data
+        }));
+        total++;
+    }
+
+    await fetcher({
+        body: JSON.stringify({
+            query: `
+                mutation CreateBatch($input: CreateBatchInput!){
+                    updateBatch(input: $input) {
+                        batch {
+                            _id
+                            total
+                        }
+                    }
+                }
+            `,
+            variables: {
+                input: {
+                    _id: batch,
+                    total
+                }
+            }
+        })
+    })
+
+    await s3.send(new S3.DeleteObjectCommand({
+        Bucket: task.Bucket,
+        Key: task.Key
+    }));
+
+    console.log('ok - extraction complete');
+}
+
+async function fetcher(body) {
     console.log('Posting metadata to API');
     const res = await fetch(params.get(`/api/url-${STAGE}`), {
         method: 'POST',
@@ -71,15 +132,7 @@ export default async function handler() {
             'Content-Type': 'application/json',
             'x-api-key': APIKEY
         },
-        body: JSON.stringify({
-            query: QUERY,
-            variables: {
-                input: {
-                    _id: batch,
-                    processing_start: new Date()
-                }
-            }
-        })
+        body
     });
 
     if (!res.ok) {
@@ -101,30 +154,7 @@ export default async function handler() {
         throw new Error(json.errors[0].message);
     }
 
-    const zip = new Zip(path.resolve(os.tmpdir(), 'input.zip'));
-
-    for (const entry of zip.getEntries()) {
-        const { ext } = path.parse(entry.entryName);
-        if (!ext) continue;
-
-        const data = entry.getData();
-        // Ensure if there are images with the same name they don't clobber on s3
-        const key = crypto.createHash('md5').update(data).digest('hex');
-
-        console.log(`ok - writing: ${batch}/${key}${ext}`);
-        await s3.send(new S3.PutObjectCommand({
-            Bucket: task.Bucket,
-            Key: `${batch}/${key}${ext}`,
-            Body: data
-        }));
-    }
-
-    await s3.send(new S3.DeleteObjectCommand({
-        Bucket: task.Bucket,
-        Key: task.Key
-    }));
-
-    console.log('ok - extraction complete');
+    return json;
 }
 
 if (import.meta.url === `file://${process.argv[1]}`) handler();
