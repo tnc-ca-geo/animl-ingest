5032896b2053a15cb2c07af99b7823590ad88376
ingalls <nick@ingalls.ca>
2023-03-28 12:02:27 -0600

    Add tests for StopBatch API

--- ingest-delete/test/basic.test.js
+++ ingest-delete/test/basic.test.js
@@ -5,7 +5,7 @@ import CloudFormation from '@aws-sdk/client-cloudformation';
 import { MockAgent, setGlobalDispatcher } from 'undici';
 import SSM from '@aws-sdk/client-ssm';
 
-test('Basic', async (t) => {
+test('Basic - CloudWatch Alarm', async (t) => {
     const mockAgent = new MockAgent();
     setGlobalDispatcher(mockAgent);
 
@@ -59,6 +59,68 @@ test('Basic', async (t) => {
                     })
                 }
             }]
+        });
+    } catch (err) {
+        t.error(err);
+    }
+
+    t.deepEquals(order, [
+        'SSM:GetParametersCommand',
+        'CloudFormation:DeleteStack'
+    ]);
+
+    Sinon.restore();
+    t.end();
+});
+
+test('Basic - StopBatch API', async (t) => {
+    const mockAgent = new MockAgent();
+    setGlobalDispatcher(mockAgent);
+
+    const mockPool = mockAgent.get('http://example.com');
+
+    mockPool.intercept({
+        path: '/',
+        method: 'POST'
+    }).reply(200, { message: 'posted' });
+
+    const order = [];
+    Sinon.stub(SSM.SSMClient.prototype, 'send').callsFake((command) => {
+        if (command instanceof SSM.GetParametersCommand) {
+            order.push('SSM:GetParametersCommand');
+
+            t.deepEquals(command.input, {
+                Names: ['/api/url-test'],
+                WithDecryption: true
+            });
+
+            return Promise.resolve({
+                Parameters: [{
+                    Name: '/api/url-test',
+                    Value: 'http://example.com'
+                }]
+            });
+        } else {
+            t.fail('Unexpected Command');
+        }
+    });
+
+    Sinon.stub(CloudFormation.CloudFormationClient.prototype, 'send').callsFake((command) => {
+        if (command instanceof CloudFormation.DeleteStackCommand) {
+            order.push('CloudFormation:DeleteStack');
+
+            t.ok(command.input.StackName.startsWith('animl-ingest-test-batch-'));
+
+            return Promise.resolve({});
+        } else {
+            t.fail('Unexpected Command');
+        }
+    });
+
+    try {
+        process.env.STAGE = 'test';
+        await IngestDelete({
+            batch: 'batch-847400c8-8e54-4a40-ab5c-0d593939c883'
         });
     } catch (err) {
         t.error(err);
